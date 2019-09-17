b7b90a06263d801b003923d80224e975f5d55a10
VAO61 <vao.frontend@gmail.com>
2019-09-17 10:32:49 +0300

    _N 0.1.2

--- src/items/interfaces/item.interface.ts
+++ src/items/interfaces/item.interface.ts
@@ -1,4 +1,8 @@
-export interface Item {
+import { Document } from 'mongoose';
+
+// export interface Cat extends Document {
+
+export interface Item extends Document {
   id?: string;
   name: string;
   description?: string;
