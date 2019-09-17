530d8116de3b96c4346258d1d54e00521d452648
VAO61 <vao.frontend@gmail.com>
2019-09-17 12:24:57 +0300

    1.0.0.1

--- config/shield.js
+++ config/shield.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 module.exports = {
   /*
@@ -27,8 +27,7 @@ module.exports = {
     | }
     |
     */
-    directives: {
-    },
+    directives: {},
     /*
     |--------------------------------------------------------------------------
     | Report only
@@ -132,7 +131,7 @@ module.exports = {
   |
   */
   csrf: {
-    enable: true,
+    enable: false,
     methods: ['POST', 'PUT', 'DELETE'],
     filterUris: [],
     cookieOptions: {
@@ -142,4 +141,4 @@ module.exports = {
       maxAge: 7200
     }
   }
-}
+};
